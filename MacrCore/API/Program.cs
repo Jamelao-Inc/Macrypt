6305b85d9620d545db25a99c47e0b06b3bd02f9d
JoaoPedroVieiraLeao <joaopedrovieiraleao@outlook.com>
2015-10-30 23:35:32 -0200

    Release 1.0 for testing
    Guys, it is a encrypt system that cannot be decrypted.

--- MacrCore/API/Program.cs
+++ MacrCore/API/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Specialized;
+using System.IO;
+
 
 namespace API
 {
@@ -10,6 +13,95 @@ namespace API
     {
         static void Main(string[] args)
         {
+
+            //Console.WriteLine(_);
+
+            /*
+            string command = "";
+            foreach(string x in args)
+            {
+                
+                {
+
+                }
+                command += x + " ";
+            }*/
+            while (true)
+            {
+                Console.WriteLine("TYPE: ");
+                cmd(Console.ReadLine());
+            } 
         }
+        static void cmd(string c)
+        {
+            if (c.Length > 0)
+            {
+                if (c=="e")
+                {
+                    Console.WriteLine("ENCRYPT ?");
+                    string toEncrypt = Console.ReadLine();
+                    string key = Console.ReadLine();
+                    encrypt(toEncrypt, key);
+                }
+            }
+        }
+
+        static void encrypt(string str, string mn)
+        {
+            string m1 = "a2bh771k033a";
+            string m2 = "tr detyi9erhydtiurhyeiu9thy48etby5957373";
+            string m3 = "gesi y59yzoi98@#$G&HH5";
+            string m4 = "aM64";
+            string m5 = "•gr6546456";
+
+            string key;
+
+            if (mn == "m1")
+            {
+                key = m1;
+            }else if (mn == "m2")
+            {
+                key = m2;
+            }else if (mn == "m3")
+            {
+                key = m3;
+            }else if (mn == "m4")
+            {
+                key = m4;
+            }else if (mn == "m5")
+            {
+                key = m5;
+            }
+            else
+            {
+                key = mn;
+            }
+            Console.WriteLine(replaceStr(str, key));
+            
+        }
+
+        private static string replaceStr(string c, string key)
+        {
+            string x = "";
+
+            for(int i=0; i < key.Length; i++)
+            {
+                x += key[i]*870;
+                for (int y = 0; y < c.Length; y++)
+                {
+                    x += c[y];
+
+                    x = x.Replace(c[y],c[y%125]);
+                    x = x.Replace(c[y], c[y % 125]);
+                    x = x.Replace(c[y], c[y % 125]);
+                    x = x.Replace(c[y], Convert.ToChar( new Random().Next(0,256)));
+                }
+
+            }
+
+            return x;
+        }
+
+
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Macrypt
 Welcome, Macrypt is a advanced cryptography system.
+There are so many complex things in Macrypt that the computer understands and we don't.
+We have actually a bit module-keys. But we want reach 256 faster as possible.
